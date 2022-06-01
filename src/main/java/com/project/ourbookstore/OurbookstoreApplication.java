051f7113144fa0078fe1dad9eee8a3103f5282b4
gaeun <knospe1@naver.com>
2022-06-01 20:45:59 +0900

    setting: basic setting

---
+++ src/main/java/com/project/ourbookstore/OurbookstoreApplication.java
@@ -0,0 +1,13 @@
+package com.project.ourbookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OurbookstoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OurbookstoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/ourbookstore/OurbookstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.ourbookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OurbookstoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
