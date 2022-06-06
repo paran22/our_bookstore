f4b4149a209447e26efc7ddd868121783a172bbf
gaeun <knospe1@naver.com>
2022-06-06 22:00:53 +0900

    fix: register bean

--- src/main/java/com/project/ourbookstore/repository/BookstoreRepositoryImpl.java
+++ src/main/java/com/project/ourbookstore/repository/BookstoreRepositoryImpl.java
@@ -3,7 +3,9 @@ package com.project.ourbookstore.repository;
 import com.project.ourbookstore.entity.Bookstore;
 import com.project.ourbookstore.service.CreateBookstore;
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
 
+@Repository
 @RequiredArgsConstructor
 public class BookstoreRepositoryImpl implements BookstoreRepository {
     private final BookstoreJpaRepository jpaRepository;
