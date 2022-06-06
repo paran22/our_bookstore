bfc54e800ef98227a4227b54b11c601756841038
gaeun <knospe1@naver.com>
2022-06-06 22:01:34 +0900

    refactor: change type from String to LocalDateTime

--- src/main/java/com/project/ourbookstore/entity/Timestamped.java
+++ src/main/java/com/project/ourbookstore/entity/Timestamped.java
@@ -1,21 +1,23 @@
 package com.project.ourbookstore.entity;
 
 import lombok.Getter;
+import org.apache.tomcat.jni.Local;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.Entity;
 import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
 
 @Getter
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
 public class Timestamped {
     @CreatedDate
-    private String createdAt;
-
+    private LocalDateTime createdAt;
     @LastModifiedDate
-    private String modifiedAt;
+    private LocalDateTime modifiedAt;
 }
